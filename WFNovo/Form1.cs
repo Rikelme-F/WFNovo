888b9813d393c60234cea6a424bbc451a3a56ab2
Rikelme-F <Rikelme.fsantos@senacsp.edu.br>
2025-02-20 21:58:55 -0300

    Adicionar arquivos de projeto.

---
+++ WFNovo/Form1.cs
@@ -0,0 +1,17 @@
+namespace WFNovo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
